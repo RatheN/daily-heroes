2835465d02d37b7df273ceeaae6378eabd3682b5
Nate <nprathe10@gmail.com>
2019-12-16 17:21:07 -0600

    added welcome

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
+  root 'welcome#home'
+  
   resources :commitments
   resources :users 
   resources :pledges do
