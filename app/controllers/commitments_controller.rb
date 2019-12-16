d8bca3afadb4a328c75e31aa03767aa3136e7114
Nate <nprathe10@gmail.com>
2019-12-16 02:34:28 -0600

    created nested? method

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,2 +1,9 @@
 class CommitmentsController < ApplicationController
+    before_action :authorize, only: [:create, :new, :edit, :update]
+    before_action :nested?, only: [:new, :index]
+
+    def nested?
+        @pledge = Pledge.find_by_id(params[:pledge_id]) if params[:pledge_id]
+    end
+    
 end
