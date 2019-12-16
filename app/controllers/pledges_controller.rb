57c2048a193e9bec549c85cf53e74cfd642d05e6
Nate <nprathe10@gmail.com>
2019-12-16 16:58:39 -0600

    added index to pledges controller

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,6 +1,10 @@
 class PledgesController < ApplicationController
     before_action :authorize, only: [:create, :new]
 
+    def index
+        @pledges = Pledge.all
+    end
+
     def new
         @pledge = Pledge.new
     end
@@ -8,5 +12,11 @@ class PledgesController < ApplicationController
     def show
         @pledge = Pledge.find_by(params[:id])
     end
-    
+
+    private
+
+    def pledge_params
+        params.require(:pledge).permit(:action)
+    end
+
 end
