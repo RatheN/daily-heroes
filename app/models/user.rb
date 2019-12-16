d47282c86b03f7633ced016734fa8fef6d408cda
Nate <nprathe10@gmail.com>
2019-12-15 22:42:00 -0600

    User relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :posts
+    has_many :locations, through: :posts
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
