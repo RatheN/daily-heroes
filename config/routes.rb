45f80bb7db9cf06a6aaea0dd4f3a82ba0ee2b418
Nate <nprathe10@gmail.com>
2019-12-16 00:28:28 -0600

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :locations
   resources :posts
-  resources :users
+  resources :users do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
