2f6e2af05da76ce7009683c91d578f9fa384ab25
Nate <nprathe10@gmail.com>
2019-12-16 00:16:34 -0600

    create and destroy edited

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          flash[:success] = "New user added."
+          redirect_to user_path(@user)
+        else
+          @error = "Login failed. Please try again."
+          render '/sessions/new'
+        end
     end
 
     def destroy
-
+        session.clear
+        redirect_to user_path
     end
 
     
