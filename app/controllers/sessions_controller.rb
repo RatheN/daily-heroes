84e46bdcfd4de2c11ad1e4c4e29bd2ac7faf7a7d
Nate <nprathe10@gmail.com>
2019-12-16 00:18:23 -0600

    omniauth added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,5 +20,12 @@ class SessionsController < ApplicationController
         redirect_to user_path
     end
 
+    def omniauth
+        @user = User.from_omniauth(auth)
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path
+    end
+
     
 end
